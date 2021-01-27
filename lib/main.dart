cdaf333ad3c55366503ee2bfea1d9e4445ddb27f
Sinem Ak <32371584+sinemmakk@users.noreply.github.com>
2021-01-27 15:41:39 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'dart:math';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-// Sinem Ak
-// 1306170075
 
 List myArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 Stopwatch mytimer = Stopwatch();
